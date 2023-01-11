cc8d9b61dcaa21d0d3c9bf7b92fdeeaaa5d4dcf3
mattes2008 <mattes2906.dev@gmail.com>
2023-01-11 22:09:34 +0100

    Internal Commit

--- plugins/optionalFunctions.js
+++ plugins/optionalFunctions.js
@@ -3,5 +3,6 @@ function fullReset () {
 	localStorage.removeItem("vocabularyCoach_mainKey");
 	localStorage.removeItem("vocabularyCoach_remainSignedIn");
 	localStorage.removeItem("vocabularyCoach_userData");
+	location.reload(true);
 
 }
